9f29dd00221746abb0d49200adf077441632cca2
veiviser <veiviser@oslo.kommune.no>
2020-09-14 10:22:39 +0200

    Brew formula update for okctl version v0.0.7

--- Formula/okctl.rb
+++ Formula/okctl.rb
@@ -2,16 +2,16 @@
 class Okctl < Formula
   desc "Opinionated and effortless infrastructure and application management"
   homepage "https://github.com/oslokommune/okctl"
-  version "0.0.6"
+  version "0.0.7"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/oslokommune/okctl/releases/download/v0.0.6/okctl_Darwin_amd64.tar.gz"
-    sha256 "375539c6451db53370e61637481ddc9302b5ab1c0ad143c6066693d1a8bff42e"
+    url "https://github.com/oslokommune/okctl/releases/download/v0.0.7/okctl_Darwin_amd64.tar.gz"
+    sha256 "6e45dc2a0a351f6ca579070299cc2f13bfab034c1b4bc9af7e274439fb7139a0"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/oslokommune/okctl/releases/download/v0.0.6/okctl_Linux_amd64.tar.gz"
-      sha256 "0db654e3ef899c356a727441ffa3b28311f98945dbb6bb1ebd47118b50282d4a"
+      url "https://github.com/oslokommune/okctl/releases/download/v0.0.7/okctl_Linux_amd64.tar.gz"
+      sha256 "aa38a6d93bca97c4591ea73edd5b28b8910b8479584fca5dae8ad4883033213b"
     end
   end
   

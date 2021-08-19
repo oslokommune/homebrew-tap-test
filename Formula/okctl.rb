513e7d7de538616a7e7f7f291994b8415bd431f4
okctl <okctl@oslo.kommune.no>
2021-08-19 12:51:18 +0200

    Brew formula update for okctl version v0.0.64

--- Formula/okctl.rb
+++ Formula/okctl.rb
@@ -5,16 +5,16 @@
 class Okctl < Formula
   desc "Opinionated and effortless infrastructure and application management"
   homepage "https://github.com/oslokommune/okctl"
-  version "0.0.63"
+  version "0.0.64"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/oslokommune/okctl/releases/download/v0.0.63/okctl_Darwin_amd64.tar.gz"
-    sha256 "2875cc7ac839a9ef02606e774f8adfec7132efe1b14a97bfbec4315505050f41"
+    url "https://github.com/oslokommune/okctl/releases/download/v0.0.64/okctl_Darwin_amd64.tar.gz"
+    sha256 "7beb6702f7cbae8a0e696e7f08c83ad0a9bef93d042c54a7d99da648913f8881"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/oslokommune/okctl/releases/download/v0.0.63/okctl_Linux_amd64.tar.gz"
-    sha256 "1c5910429b1ac5eb175109d49bde3ab5fe6223443d0b1217a400077c6c131c07"
+    url "https://github.com/oslokommune/okctl/releases/download/v0.0.64/okctl_Linux_amd64.tar.gz"
+    sha256 "2cd1124cf1846fd4cec1e33798235452ddc2d67a4a2b149f863bb8ba552d9f42"
   end
 
   head "https://github.com/oslokommune/okctl/releases/download/latest_release/okctl_Darwin_amd64.tar.gz"
